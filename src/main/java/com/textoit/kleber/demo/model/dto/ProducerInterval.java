ac7a988c122f951edb24fdae4c7fd40e1eed2c19
Kleber <kleber.damasco@terceiros.calcard.com.br>
2020-04-10 15:53:18 -0300

    Ajustes finais

--- src/main/java/com/textoit/kleber/demo/model/dto/ProducerInterval.java
+++ src/main/java/com/textoit/kleber/demo/model/dto/ProducerInterval.java
@@ -2,18 +2,14 @@ package com.textoit.kleber.demo.model.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
-@Builder
-public class ProducerMostInterval {
-
+public class ProducerInterval {
     private String producer;
     private Integer interval;
     private Integer previousWin;
     private Integer followingWin;
-
-
 }
---
+++ src/main/java/com/textoit/kleber/demo/model/dto/ProducerIntervals.java
@@ -0,0 +1,21 @@
+package com.textoit.kleber.demo.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Builder
+@Getter
+@Setter
+public class ProducerIntervals {
+
+    @JsonProperty("min")
+    private List<ProducerInterval> minIntervalList;
+
+    @JsonProperty("max")
+    private List<ProducerInterval> maxIntervalList;
+
+}
